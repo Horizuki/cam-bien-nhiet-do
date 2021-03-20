767330adc46a1b4d36355dc50c6b2aa2617b848c
Mira <65954785+IkaShirozuki@users.noreply.github.com>
2021-03-20 16:38:08 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,24 @@
-let NHIETDO = 0
 NPNLCD.LcdInit()
 NPNLCD.ShowString("Xin chao", 0, 0)
 basic.forever(function () {
-    NHIETDO = MLX90614.temperature(TemperatureLocation.Object) + 3.1
-    NPNLCD.clear()
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
-    if (NHIETDO <= 35) {
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        NPNLCD.ShowString("MOI TRUONG:" + NHIETDO, 0, 0)
-        NPNLCD.ShowString("KHI HAU TOT!", 2, 1)
-    } else {
-        if (NHIETDO <= 38) {
-            pins.digitalWritePin(DigitalPin.P1, 0)
-            pins.digitalWritePin(DigitalPin.P2, 1)
-            music.playTone(988, music.beat(BeatFraction.Whole))
-            NPNLCD.ShowString("NHIET DO: " + NHIETDO, 0, 0)
-            NPNLCD.ShowString("HEN GAP LAI!", 2, 1)
-        } else {
-            NPNLCD.ShowString("NHIET DO: " + NHIETDO, 0, 0)
-            NPNLCD.ShowString("NGUY HIEM!", 3, 1)
-            for (let index = 0; index <= 9; index++) {
-                pins.digitalWritePin(DigitalPin.P1, 1)
-                pins.digitalWritePin(DigitalPin.P2, 0)
-                music.playTone(988, music.beat(BeatFraction.Whole))
-                pins.digitalWritePin(DigitalPin.P1, 0)
-                music.rest(music.beat(BeatFraction.Whole))
-                basic.pause(10)
-            }
-        }
-    }
     basic.pause(1000)
+    NPNLCD.LcdInit()
+    NPNLCD.ShowString("七七", 0, 0)
+    basic.pause(1000)
+    NPNLCD.ShowString("一千只猫薄禾", 0, 0)
+    basic.pause(1000)
+    NPNLCD.clear()
 })
